4d0699f1f6f1fcbd844545852118ec36e75ba796
Yuntian <yuntiand@cs.cmu.edu>
2014-10-01 11:29:59 -0400

    writing fscanf

--- src/matrix_loader.cpp
+++ src/matrix_loader.cpp
@@ -1,13 +1,25 @@
 #include <string>
 #include <vector>
-#include <atomic>
+#include <stdio>
 #include "matrix_loader.hpp"
 
 namespace sparsecoding {
 MatrixLoader::MatrixLoader() {
 }
 void MatrixLoader::Load(std::string data_file, int client_id) {
+    FILE * fp;
+    fp = fopen(data_file.c_str(), 'r');
+    fscanf("%d%d", &m, &n);
 }
 MatrixLoader::~MatrixLoader() {
 }
+int MatrixLoader::GetM() {
+    return m;
+}
+int MatrixLoader::GetClientN() {
+    return client_n;
+}
+std::vector<float> MatrixLoader::GetData(int i){
+}
+
 }
--- src/matrix_loader.hpp
+++ src/matrix_loader.hpp
@@ -9,7 +9,12 @@ class MatrixLoader {
         MatrixLoader();
         ~MatrixLoader();
         void Load(std::string data_file, int client_id);
+        int GetM();
+        int GetClientN();
+        std::vector<float> GetData(int i);
     private:
-        std::atomic<std::vector<float> > data;
+        std::vector<std::vector<float> > data;
+        int m, n;
+        int client_n;
 };
 };
